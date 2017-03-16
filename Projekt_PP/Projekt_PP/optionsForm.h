7defbd8dda7faa6c0496da778df29b8c33d11a02
Marcel <mar_woz95@outlook.com>
2017-03-16 23:25:20 +0100

    new view in option

--- Projekt_PP/Projekt_PP/optionsForm.h
+++ Projekt_PP/Projekt_PP/optionsForm.h
@@ -61,6 +61,14 @@ namespace Projekt_PP {
 	private: System::Windows::Forms::TextBox^  textBox7;
 	private: System::Windows::Forms::Button^  butApplySDA;
 	private: System::Windows::Forms::Button^  butApplySize;
+	private: System::Windows::Forms::Panel^  panel1;
+	private: System::Windows::Forms::Panel^  panel2;
+	private: System::Windows::Forms::Panel^  panel6;
+	private: System::Windows::Forms::Panel^  panel5;
+	private: System::Windows::Forms::Panel^  panel3;
+	private: System::Windows::Forms::Panel^  panel4;
+	private: System::Windows::Forms::Panel^  panel7;
+	private: System::Windows::Forms::Panel^  panel8;
 
 	private:
 		/// <summary>
@@ -101,20 +109,33 @@ namespace Projekt_PP {
 			this->textBox7 = (gcnew System::Windows::Forms::TextBox());
 			this->butApplySDA = (gcnew System::Windows::Forms::Button());
 			this->butApplySize = (gcnew System::Windows::Forms::Button());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
+			this->panel4 = (gcnew System::Windows::Forms::Panel());
+			this->panel5 = (gcnew System::Windows::Forms::Panel());
+			this->panel6 = (gcnew System::Windows::Forms::Panel());
+			this->panel7 = (gcnew System::Windows::Forms::Panel());
+			this->panel8 = (gcnew System::Windows::Forms::Panel());
+			this->panel1->SuspendLayout();
+			this->panel2->SuspendLayout();
+			this->panel4->SuspendLayout();
+			this->panel7->SuspendLayout();
+			this->panel8->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// sliceNameTextBox
 			// 
-			this->sliceNameTextBox->Location = System::Drawing::Point(1, 17);
+			this->sliceNameTextBox->Location = System::Drawing::Point(9, 17);
 			this->sliceNameTextBox->Name = L"sliceNameTextBox";
-			this->sliceNameTextBox->Size = System::Drawing::Size(100, 20);
+			this->sliceNameTextBox->Size = System::Drawing::Size(77, 20);
 			this->sliceNameTextBox->TabIndex = 0;
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
 			this->label1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
-			this->label1->Location = System::Drawing::Point(12, -1);
+			this->label1->Location = System::Drawing::Point(16, -1);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(63, 15);
 			this->label1->TabIndex = 1;
@@ -122,7 +143,7 @@ namespace Projekt_PP {
 			// 
 			// butApplySliceName
 			// 
-			this->butApplySliceName->Location = System::Drawing::Point(12, 43);
+			this->butApplySliceName->Location = System::Drawing::Point(9, 43);
 			this->butApplySliceName->Name = L"butApplySliceName";
 			this->butApplySliceName->Size = System::Drawing::Size(75, 23);
 			this->butApplySliceName->TabIndex = 2;
@@ -132,7 +153,7 @@ namespace Projekt_PP {
 			// chBoxNegative
 			// 
 			this->chBoxNegative->AutoSize = true;
-			this->chBoxNegative->Location = System::Drawing::Point(1, 94);
+			this->chBoxNegative->Location = System::Drawing::Point(12, 15);
 			this->chBoxNegative->Name = L"chBoxNegative";
 			this->chBoxNegative->Size = System::Drawing::Size(69, 17);
 			this->chBoxNegative->TabIndex = 3;
@@ -142,7 +163,7 @@ namespace Projekt_PP {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(157, 17);
+			this->textBox1->Location = System::Drawing::Point(34, 17);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(100, 20);
 			this->textBox1->TabIndex = 4;
@@ -150,7 +171,7 @@ namespace Projekt_PP {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(177, 1);
+			this->label2->Location = System::Drawing::Point(69, 0);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(34, 13);
 			this->label2->TabIndex = 5;
@@ -158,7 +179,7 @@ namespace Projekt_PP {
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(71, 130);
+			this->textBox2->Location = System::Drawing::Point(68, 51);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(45, 20);
 			this->textBox2->TabIndex = 6;
@@ -166,7 +187,7 @@ namespace Projekt_PP {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(9, 133);
+			this->label3->Location = System::Drawing::Point(6, 54);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(54, 13);
 			this->label3->TabIndex = 7;
@@ -174,7 +195,7 @@ namespace Projekt_PP {
 			// 
 			// butApplyScale
 			// 
-			this->butApplyScale->Location = System::Drawing::Point(158, 43);
+			this->butApplyScale->Location = System::Drawing::Point(48, 44);
 			this->butApplyScale->Name = L"butApplyScale";
 			this->butApplyScale->Size = System::Drawing::Size(75, 23);
 			this->butApplyScale->TabIndex = 8;
@@ -183,7 +204,7 @@ namespace Projekt_PP {
 			// 
 			// butApplyThreshold
 			// 
-			this->butApplyThreshold->Location = System::Drawing::Point(158, 127);
+			this->butApplyThreshold->Location = System::Drawing::Point(155, 48);
 			this->butApplyThreshold->Name = L"butApplyThreshold";
 			this->butApplyThreshold->Size = System::Drawing::Size(75, 23);
 			this->butApplyThreshold->TabIndex = 9;
@@ -193,7 +214,7 @@ namespace Projekt_PP {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(9, 176);
+			this->label4->Location = System::Drawing::Point(6, 6);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(73, 13);
 			this->label4->TabIndex = 10;
@@ -201,28 +222,28 @@ namespace Projekt_PP {
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(71, 207);
+			this->textBox3->Location = System::Drawing::Point(68, 37);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(45, 20);
 			this->textBox3->TabIndex = 11;
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(71, 233);
+			this->textBox4->Location = System::Drawing::Point(68, 63);
 			this->textBox4->Name = L"textBox4";
 			this->textBox4->Size = System::Drawing::Size(45, 20);
 			this->textBox4->TabIndex = 12;
 			// 
 			// textBox5
 			// 
-			this->textBox5->Location = System::Drawing::Point(71, 259);
+			this->textBox5->Location = System::Drawing::Point(68, 89);
 			this->textBox5->Name = L"textBox5";
 			this->textBox5->Size = System::Drawing::Size(45, 20);
 			this->textBox5->TabIndex = 13;
 			// 
 			// textBox6
 			// 
-			this->textBox6->Location = System::Drawing::Point(71, 285);
+			this->textBox6->Location = System::Drawing::Point(68, 115);
 			this->textBox6->Name = L"textBox6";
 			this->textBox6->Size = System::Drawing::Size(45, 20);
 			this->textBox6->TabIndex = 14;
@@ -230,7 +251,7 @@ namespace Projekt_PP {
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(9, 210);
+			this->label5->Location = System::Drawing::Point(6, 40);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(40, 13);
 			this->label5->TabIndex = 15;
@@ -239,7 +260,7 @@ namespace Projekt_PP {
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(9, 236);
+			this->label6->Location = System::Drawing::Point(6, 66);
 			this->label6->Name = L"label6";
 			this->label6->Size = System::Drawing::Size(54, 13);
 			this->label6->TabIndex = 16;
@@ -248,7 +269,7 @@ namespace Projekt_PP {
 			// label7
 			// 
 			this->label7->AutoSize = true;
-			this->label7->Location = System::Drawing::Point(9, 262);
+			this->label7->Location = System::Drawing::Point(6, 92);
 			this->label7->Name = L"label7";
 			this->label7->Size = System::Drawing::Size(61, 13);
 			this->label7->TabIndex = 17;
@@ -257,7 +278,7 @@ namespace Projekt_PP {
 			// label8
 			// 
 			this->label8->AutoSize = true;
-			this->label8->Location = System::Drawing::Point(9, 288);
+			this->label8->Location = System::Drawing::Point(6, 118);
 			this->label8->Name = L"label8";
 			this->label8->Size = System::Drawing::Size(29, 13);
 			this->label8->TabIndex = 18;
@@ -266,7 +287,7 @@ namespace Projekt_PP {
 			// chBoxMode
 			// 
 			this->chBoxMode->AutoSize = true;
-			this->chBoxMode->Location = System::Drawing::Point(158, 209);
+			this->chBoxMode->Location = System::Drawing::Point(155, 39);
 			this->chBoxMode->Name = L"chBoxMode";
 			this->chBoxMode->Size = System::Drawing::Size(53, 17);
 			this->chBoxMode->TabIndex = 19;
@@ -276,7 +297,7 @@ namespace Projekt_PP {
 			// chBoxFast_Slow
 			// 
 			this->chBoxFast_Slow->AutoSize = true;
-			this->chBoxFast_Slow->Location = System::Drawing::Point(158, 236);
+			this->chBoxFast_Slow->Location = System::Drawing::Point(155, 66);
 			this->chBoxFast_Slow->Name = L"chBoxFast_Slow";
 			this->chBoxFast_Slow->Size = System::Drawing::Size(74, 17);
 			this->chBoxFast_Slow->TabIndex = 20;
@@ -286,7 +307,7 @@ namespace Projekt_PP {
 			// label9
 			// 
 			this->label9->AutoSize = true;
-			this->label9->Location = System::Drawing::Point(9, 324);
+			this->label9->Location = System::Drawing::Point(4, 1);
 			this->label9->Name = L"label9";
 			this->label9->Size = System::Drawing::Size(52, 13);
 			this->label9->TabIndex = 21;
@@ -295,7 +316,7 @@ namespace Projekt_PP {
 			// label10
 			// 
 			this->label10->AutoSize = true;
-			this->label10->Location = System::Drawing::Point(9, 352);
+			this->label10->Location = System::Drawing::Point(19, 28);
 			this->label10->Name = L"label10";
 			this->label10->Size = System::Drawing::Size(27, 13);
 			this->label10->TabIndex = 22;
@@ -303,14 +324,14 @@ namespace Projekt_PP {
 			// 
 			// textBox7
 			// 
-			this->textBox7->Location = System::Drawing::Point(71, 349);
+			this->textBox7->Location = System::Drawing::Point(66, 25);
 			this->textBox7->Name = L"textBox7";
 			this->textBox7->Size = System::Drawing::Size(45, 20);
 			this->textBox7->TabIndex = 23;
 			// 
 			// butApplySDA
 			// 
-			this->butApplySDA->Location = System::Drawing::Point(157, 282);
+			this->butApplySDA->Location = System::Drawing::Point(154, 112);
 			this->butApplySDA->Name = L"butApplySDA";
 			this->butApplySDA->Size = System::Drawing::Size(75, 23);
 			this->butApplySDA->TabIndex = 24;
@@ -319,54 +340,135 @@ namespace Projekt_PP {
 			// 
 			// butApplySize
 			// 
-			this->butApplySize->Location = System::Drawing::Point(158, 342);
+			this->butApplySize->Location = System::Drawing::Point(150, 24);
 			this->butApplySize->Name = L"butApplySize";
 			this->butApplySize->Size = System::Drawing::Size(75, 23);
 			this->butApplySize->TabIndex = 25;
 			this->butApplySize->Text = L"Apply";
 			this->butApplySize->UseVisualStyleBackColor = true;
 			// 
+			// panel1
+			// 
+			this->panel1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel1->Controls->Add(this->butApplySDA);
+			this->panel1->Controls->Add(this->chBoxFast_Slow);
+			this->panel1->Controls->Add(this->chBoxMode);
+			this->panel1->Controls->Add(this->label8);
+			this->panel1->Controls->Add(this->label7);
+			this->panel1->Controls->Add(this->label6);
+			this->panel1->Controls->Add(this->label5);
+			this->panel1->Controls->Add(this->textBox6);
+			this->panel1->Controls->Add(this->textBox5);
+			this->panel1->Controls->Add(this->textBox4);
+			this->panel1->Controls->Add(this->textBox3);
+			this->panel1->Controls->Add(this->label4);
+			this->panel1->Location = System::Drawing::Point(1, 170);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(269, 154);
+			this->panel1->TabIndex = 26;
+			// 
+			// panel2
+			// 
+			this->panel2->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel2->Controls->Add(this->panel6);
+			this->panel2->Controls->Add(this->panel5);
+			this->panel2->Controls->Add(this->panel3);
+			this->panel2->Controls->Add(this->butApplySliceName);
+			this->panel2->Controls->Add(this->label1);
+			this->panel2->Controls->Add(this->sliceNameTextBox);
+			this->panel2->Location = System::Drawing::Point(1, 0);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(113, 76);
+			this->panel2->TabIndex = 27;
+			// 
+			// panel3
+			// 
+			this->panel3->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel3->Location = System::Drawing::Point(100, -2);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(165, 75);
+			this->panel3->TabIndex = 3;
+			// 
+			// panel4
+			// 
+			this->panel4->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel4->Controls->Add(this->butApplyScale);
+			this->panel4->Controls->Add(this->label2);
+			this->panel4->Controls->Add(this->textBox1);
+			this->panel4->Location = System::Drawing::Point(103, 0);
+			this->panel4->Name = L"panel4";
+			this->panel4->Size = System::Drawing::Size(166, 76);
+			this->panel4->TabIndex = 28;
+			// 
+			// panel5
+			// 
+			this->panel5->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel5->Location = System::Drawing::Point(-4, 72);
+			this->panel5->Name = L"panel5";
+			this->panel5->Size = System::Drawing::Size(269, 96);
+			this->panel5->TabIndex = 4;
+			// 
+			// panel6
+			// 
+			this->panel6->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel6->Location = System::Drawing::Point(-4, 73);
+			this->panel6->Name = L"panel6";
+			this->panel6->Size = System::Drawing::Size(268, 94);
+			this->panel6->TabIndex = 5;
+			// 
+			// panel7
+			// 
+			this->panel7->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel7->Controls->Add(this->butApplyThreshold);
+			this->panel7->Controls->Add(this->label3);
+			this->panel7->Controls->Add(this->textBox2);
+			this->panel7->Controls->Add(this->chBoxNegative);
+			this->panel7->Location = System::Drawing::Point(1, 79);
+			this->panel7->Name = L"panel7";
+			this->panel7->Size = System::Drawing::Size(268, 89);
+			this->panel7->TabIndex = 29;
+			// 
+			// panel8
+			// 
+			this->panel8->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel8->Controls->Add(this->butApplySize);
+			this->panel8->Controls->Add(this->textBox7);
+			this->panel8->Controls->Add(this->label10);
+			this->panel8->Controls->Add(this->label9);
+			this->panel8->Location = System::Drawing::Point(1, 324);
+			this->panel8->Name = L"panel8";
+			this->panel8->Size = System::Drawing::Size(268, 60);
+			this->panel8->TabIndex = 30;
+			// 
 			// optionsForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(519, 385);
-			this->Controls->Add(this->butApplySize);
-			this->Controls->Add(this->butApplySDA);
-			this->Controls->Add(this->textBox7);
-			this->Controls->Add(this->label10);
-			this->Controls->Add(this->label9);
-			this->Controls->Add(this->chBoxFast_Slow);
-			this->Controls->Add(this->chBoxMode);
-			this->Controls->Add(this->label8);
-			this->Controls->Add(this->label7);
-			this->Controls->Add(this->label6);
-			this->Controls->Add(this->label5);
-			this->Controls->Add(this->textBox6);
-			this->Controls->Add(this->textBox5);
-			this->Controls->Add(this->textBox4);
-			this->Controls->Add(this->textBox3);
-			this->Controls->Add(this->label4);
-			this->Controls->Add(this->butApplyThreshold);
-			this->Controls->Add(this->butApplyScale);
-			this->Controls->Add(this->label3);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->chBoxNegative);
-			this->Controls->Add(this->butApplySliceName);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->sliceNameTextBox);
+			this->ClientSize = System::Drawing::Size(270, 385);
+			this->Controls->Add(this->panel8);
+			this->Controls->Add(this->panel4);
+			this->Controls->Add(this->panel2);
+			this->Controls->Add(this->panel7);
+			this->Controls->Add(this->panel1);
 			this->Name = L"optionsForm";
 			this->Text = L"optionsForm";
 			this->Load += gcnew System::EventHandler(this, &optionsForm::optionsForm_Load);
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			this->panel2->ResumeLayout(false);
+			this->panel2->PerformLayout();
+			this->panel4->ResumeLayout(false);
+			this->panel4->PerformLayout();
+			this->panel7->ResumeLayout(false);
+			this->panel7->PerformLayout();
+			this->panel8->ResumeLayout(false);
+			this->panel8->PerformLayout();
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void optionsForm_Load(System::Object^  sender, System::EventArgs^  e) {
-		MessageBox::Show("dupa");
+		
 	}
 	
 	private: System::Void chBoxNegative_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
